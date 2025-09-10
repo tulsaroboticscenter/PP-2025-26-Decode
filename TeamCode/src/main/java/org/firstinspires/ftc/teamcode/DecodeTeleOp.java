4ed178550340f31ae4e56950c02f3a0d44386787
Ace <85533979+lukemurdock@users.noreply.github.com>
2025-09-10 01:44:00 -0500

    Update DecodeTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeTeleOp.java
@@ -1,35 +1,3 @@
-/*
- * Copyright (c) 2025 FIRST
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to
- * endorse or promote products derived from this software without specific prior
- * written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
- * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -55,21 +23,16 @@ public class DecodeTeleOp extends LinearOpMode {
         while(opModeIsActive()) {
             ops.StrafeDrive(gamepad1.left_stick_y, gamepad1.right_stick_x, gamepad1.left_stick_x);
 
-            /*
-             * Here we give the user control of the speed of the launcher motor without automatically
-             * queuing a shot.
-             */
+            // Y spins up launching motor, which then enables launching.
             if (gamepad1.y) {
                 robot.launcher.setVelocity(robot.LAUNCHER_TARGET_VELOCITY);
             } else if (gamepad1.b) { // stop flywheel
                 robot.launcher.setVelocity(robot.STOP_SPEED);
             }
 
-            /*
-             * Now we call our "Launch" function.
-             */
-
             if (gamepad1.right_trigger > 0.5) {
+                // robot will not launch an artifact until the launching motor's velocity
+                // meets or exceeds HWProfile.LAUNCHER_MINIMUM_VELOCITY
                 ops.launch();
                 robot.feederTimer.reset();
             }
@@ -78,16 +41,14 @@ public class DecodeTeleOp extends LinearOpMode {
                 ops.stopLaunch();
             }
 
-            /*
-             * Show the state and motor powers
-             */
-
+            telemetry.addData("Drive/Strafe: ", "Left Stick");
+            telemetry.addData("Rotate: ", "Right Stick");
+            telemetry.addData("Spin Up Launcher: ", "Y / Triangle");
+            telemetry.addData("Spin Down Launcher: ", "B / Circle");
+            telemetry.addData("Fire (Only available when spun up): ", "RT / R3");
             telemetry.addData("launcher velocity: ", robot.launcher.getVelocity());
             telemetry.addData("feed timer (in seconds): ", robot.feederTimer.seconds());
             telemetry.update();
-
-
-
         }
     }
 }
