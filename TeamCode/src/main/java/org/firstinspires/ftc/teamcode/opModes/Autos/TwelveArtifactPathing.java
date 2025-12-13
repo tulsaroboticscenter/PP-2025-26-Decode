219a7cd60d1152afd5ed8aa2223181691a692017
Anthony10355 <25atpollard@bishopkelley.org>
2025-12-13 17:32:42 -0600

    Pedro Pathing Pathing 12 artifact auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/Autos/TwelveArtifactPathing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/Autos/TwelveArtifactPathing.java
@@ -1,105 +1,197 @@
 package org.firstinspires.ftc.teamcode.opModes.Autos;
 
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.follower;
-
 import static java.lang.Thread.sleep;
 
+import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
 import com.pedropathing.paths.Path;
+import com.pedropathing.paths.PathChain;
+import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
-import org.firstinspires.ftc.teamcode.Hardware.HWProfile;
-import org.firstinspires.ftc.teamcode.Libraries.Field;
-import org.firstinspires.ftc.teamcode.Libraries.GamepadEffects;
-import org.firstinspires.ftc.teamcode.Libraries.MechOps;
-import org.firstinspires.ftc.teamcode.Libraries.RGBLightController;
-import org.firstinspires.ftc.teamcode.Libraries.Targeting;
-import org.firstinspires.ftc.teamcode.Libraries.TurretTargeting;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
 @Autonomous(name = "RAAAAHHHH", group = "Autonomous")
 public class TwelveArtifactPathing extends OpMode {
 
-    /**
+    private Follower follower;
+    private Timer pathTimer, actionTimer, opmodeTimer;
 
-     This PedroPathing Auto is used to test before updating pathing on other Pedro autos
+    private int pathState;
 
-     **/
+    private final Pose startPose = new Pose(23.7, 129.2, Math.toRadians(144));
+    private final Pose scorePose = new Pose(59.9, 84.1, Math.toRadians(180));
+    private final Pose intake1 = new Pose(16.3, 84.1, Math.toRadians(180));
+    private final Pose prepIntake2 = new Pose(42.8, 59.7, Math.toRadians(180));
+    private final Pose intake2 = new Pose(12.6, 59.5, Math.toRadians(180));
+    private final Pose prepIntake3 = new Pose(42.8, 35.8, Math.toRadians(180));
+    private final Pose intake3 = new Pose(12.6, 35.8, Math.toRadians(180));
+    private final Pose park = new Pose(59.7, 103.5);
 
-    private enum AutoState {
-        AIMING,
-        WAIT_TO_FIRE,
-        FIRING,
-        MOVING,
-        DONE
-    }
-
-    private AutoState currentState = AutoState.AIMING;
-
-    private final Pose startPose = new Pose(60,8, Math.toRadians(90));
-    private final Pose parkPose = new Pose(60,36, Math.toRadians(180));
-
-    private Path forwards;
-    private Path backwards;
-
-    HWProfile robot = new HWProfile();
-    MechOps ops = new MechOps(robot, this);
-    Targeting target = new Targeting(robot);
-    TurretTargeting turret = new TurretTargeting(robot, target);
-
-    Pose2D goalPosition = Field.toPedro2D(Field.blueGoal);
-
-    ElapsedTime timer = new ElapsedTime();
+    private PathChain scorePreload, intakeLine1, scoreLine1, lineupIntake2, intakeLine2, scoreLine2, lineupIntake3, intakeLine3, scoreLine3;
 
     public void buildPaths() {
-        forwards = new Path(new BezierLine(startPose, parkPose));
-        forwards.setLinearHeadingInterpolation(startPose.getHeading(), parkPose.getHeading());
+        scorePreload = follower.pathBuilder()
+                .addPath(new BezierLine(startPose, scorePose))
+                .setLinearHeadingInterpolation(startPose.getHeading(), scorePose.getHeading())
+                .build();
+
+        intakeLine1 = follower.pathBuilder()
+                .addPath(new BezierLine(scorePose, intake1))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
+
+        scoreLine1 = follower.pathBuilder()
+                .addPath(new BezierLine(intake1, scorePose))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
+
+        lineupIntake2 = follower.pathBuilder()
+                .addPath(new BezierLine(scorePose, prepIntake2))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
+
+        intakeLine2 = follower.pathBuilder()
+                .addPath(new BezierLine(prepIntake2, intake2))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
+
+        scoreLine2 = follower.pathBuilder()
+                .addPath(new BezierLine(intake2, scorePose))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
+
+        lineupIntake3 = follower.pathBuilder()
+                .addPath(new BezierLine(scorePose, prepIntake3))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
+
+        intakeLine3 = follower.pathBuilder()
+                .addPath(new BezierLine(prepIntake3, intake3))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
+
+        scoreLine3 = follower.pathBuilder()
+                .addPath(new BezierLine(intake3, park))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
     }
 
-    @Override
-    public void init()
-    {
-        robot.initPedro(hardwareMap, false);
-        ops.setRGB(0.611);
-        ops.setRGBMode(RGBLightController.LEDMode.PULSE_WAKE);
-        ops.updateRGB();
-        gamepad1.runLedEffect(GamepadEffects.wakeBlue);
+    public void autonomousPathUpdate() {
+        switch (pathState) {
+            case 0:
+                follower.followPath(scorePreload, true);
+                setPathState(1);
+                break;
 
+            case 1:
+                if (!follower.isBusy()) {
+                    follower.followPath(intakeLine1, true);
+                    setPathState(2);
+                }
+                break;
+
+            case 2:
+                if (!follower.isBusy()) {
+                    follower.followPath(scoreLine1, true);
+                    setPathState(3);
+                }
+                break;
+
+            case 3:
+                if (!follower.isBusy()) {
+                    follower.followPath(lineupIntake2, true);
+                    setPathState(4);
+                }
+                break;
+
+            case 4:
+                if (!follower.isBusy()) {
+                    follower.followPath(intakeLine2, true);
+                    setPathState(5);
+                }
+                break;
+
+            case 5:
+                if (!follower.isBusy()) {
+                    follower.followPath(scoreLine2, true);
+                    setPathState(6);
+                }
+                break;
+
+            case 6:
+                if (!follower.isBusy()) {
+                    follower.followPath(lineupIntake3, true);
+                    setPathState(7);
+                }
+                break;
+
+            case 7:
+                if (!follower.isBusy()) {
+                    follower.followPath(intakeLine3, true);
+                    setPathState(8);
+                }
+                break;
+
+            case 8:
+                if (!follower.isBusy()) {
+                    follower.followPath(scoreLine3, true);
+                    setPathState(9);
+                }
+                break;
+
+            case 9:
+                if (!follower.isBusy()) {
+                    setPathState(-1);
+                }
+                break;
+        }
+    }
+
+    /** These change the states of the paths and actions. It will also reset the timers of the individual switches **/
+    public void setPathState(int pState) {
+        pathState = pState;
+        pathTimer.resetTimer();
+    }
+
+    /** This is the main loop of the OpMode, it will run repeatedly after clicking "Play". **/
+    @Override
+    public void loop() {
+        // These loop the movements of the robot, these must be called continuously in order to work
+        follower.update();
+        autonomousPathUpdate();
+        // Feedback to Driver Hub for debugging
+        telemetry.addData("path state", pathState);
+        telemetry.addData("x", follower.getPose().getX());
+        telemetry.addData("y", follower.getPose().getY());
+        telemetry.addData("heading", Math.toDegrees(follower.getPose().getHeading()));
+        telemetry.update();
+    }
+
+    /** This method is called once at the start of the OpMode.
+     * It runs all the setup actions, including building paths and starting the path system **/
+    @Override
+    public void start() {
+        opmodeTimer.resetTimer();
+        setPathState(0);
+    }
+
+    /** This method is called once at the init of the OpMode. **/
+    @Override
+    public void init() {
+        pathTimer = new Timer();
+        opmodeTimer = new Timer();
+        opmodeTimer.resetTimer();
         follower = Constants.createFollower(hardwareMap);
-        follower.setStartingPose(Field.toPedro(Field.redSmallZone));
+        buildPaths();
+        follower.setStartingPose(startPose);
 
-        robot.turret.setTarget(follower.getPose(), goalPosition);
-    }
-
-    /** This initializes the Follower and creates the forward and backward Paths. */
-    @Override
-    public void init_loop()
-    {
-        follower.update();
-        ops.updateRGB();
-    }
-
-    @Override
-    public void start()
-    {
-
-    }
-
-    /** This runs the OpMode, updating the Follower as well as printing out the debug statements to the Telemetry */
-    @Override
-    public void loop()
-    {
-        follower.update();
-        robot.turret.update();
-
-        follower.followPath(forwards, false);
+        telemetry.addData("path state", pathState);
+        telemetry.addData("x", follower.getPose().getX());
+        telemetry.addData("y", follower.getPose().getY());
+        telemetry.addData("heading", Math.toDegrees(follower.getPose().getHeading()));
+        telemetry.update();
     }
 }
-
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -19,16 +19,18 @@ public class Constants {
             .mass(10.20583) // Mass in kilograms.
             .forwardZeroPowerAcceleration(-29.16455519)
             .lateralZeroPowerAcceleration(-64.3027904)
-            .translationalPIDFCoefficients(new PIDFCoefficients(0.04,0,0.005,0.05))
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.04,0,0.005,0.01))
             .headingPIDFCoefficients(new PIDFCoefficients(0.33,0,0.02,0.02))
-            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.003,0,0.0003,0,0.01));
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.003,0,0.00007,0,0.03));
 
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 0, 1);
+    public static PathConstraints pathConstraints = new PathConstraints(
+            0.99,
+            100,
+            1,
+            1);
 
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1)
-            .xVelocity(86.35606948972688)
-            .yVelocity(70.51315571942668)
             .rightFrontMotorName("driveRF")
             .rightRearMotorName("driveRR")
             .leftRearMotorName("driveLR")
@@ -36,11 +38,13 @@ public class Constants {
             .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
             .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .xVelocity(87.82768477792816)
+            .yVelocity(69.22337521530513);
 
     public static PinpointConstants localizerConstants = new PinpointConstants()
-            .forwardPodY(-7.5)
-            .strafePodX(3)
+            .forwardPodY(-3)
+            .strafePodX(-5.5)
             .distanceUnit(DistanceUnit.INCH)
             .hardwareMapName("pinpoint")
             .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
