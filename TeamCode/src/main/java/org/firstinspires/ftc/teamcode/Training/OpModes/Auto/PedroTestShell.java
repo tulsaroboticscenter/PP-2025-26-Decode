4358a821333a05d90220e73390774a01036cb283
jgriffin0622 <41241607+jgriffin0622@users.noreply.github.com>
2026-07-29 09:50:45 -0500

    updated pedro pathing shell for park

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Training/OpModes/Auto/PedroTestShell.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Training/OpModes/Auto/PedroTestShell.java
@@ -13,27 +13,32 @@ import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
 @Autonomous
 public class PedroTestShell extends OpMode {
-
+// declare follower & path timer
     private Follower follower;
     private Timer pathTimer, opModeTimer;
     public HardwareManager hwMgr = new HardwareManager(hardwareMap);
 
-
+// create states by paths & actions
     public enum PathState {
          DRIVE_START_TO_FIRSTPOSITION,
         DRIVE_TO_SECONDPOSITION,
         ACTION_FIRST_POSITION,
-        DRIVE_TO_ENDPOSITION
+        DRIVE_TO_ENDPOSITION,
+        PARK
     }
 
     private PathState pathState;
 
+    // declare poses for each path position
     private final Pose startPosition = new Pose(94.50173310225304,3.773830155979213,Math.toRadians(90));
     private final Pose firstPosition = new Pose(94.50173310225304,46.83968804159445,Math.toRadians(90));
     private final Pose secondPosition = new Pose(70.51707362133854,70.40775472944924);
     private final Pose endPosition = new Pose(94.75800391781453,93.79117400061122);
+
+    // declare pathchains for each path
     private PathChain driveStartToFirst, driveFirstToSecond,driveSecondToEnd;
 
+    // build paths for each path chain
     public void buildPaths(){
         driveStartToFirst = follower.pathBuilder()
                 .addPath(new BezierLine(startPosition,firstPosition))
@@ -72,6 +77,12 @@ public class PedroTestShell extends OpMode {
             case DRIVE_TO_ENDPOSITION:
                 if (!follower.isBusy()){
                     follower.followPath(driveSecondToEnd);
+                    setPathState(PathState.PARK);
+                }
+                break;
+            case PARK:
+                if (!follower.isBusy()){
+                    stop();
                 }
                 break;
             default:
